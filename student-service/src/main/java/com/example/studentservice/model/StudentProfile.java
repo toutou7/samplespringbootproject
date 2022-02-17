9f61d572cab3ae0678ab7489e46ea80a31babeb7
toutou7 <67310391+toutou7@users.noreply.github.com>
2022-02-17 16:33:04 +0700

    Update StudentProfile.java

--- student-service/src/main/java/com/example/studentservice/model/StudentProfile.java
+++ student-service/src/main/java/com/example/studentservice/model/StudentProfile.java
@@ -2,8 +2,8 @@ package com.example.studentservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
