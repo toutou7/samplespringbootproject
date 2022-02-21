58c12874c5af564be8d4840f7babccf93e55a260
toutou7 <yangvitou999@gmail.com>
2022-02-21 14:44:35 +0700

    Update StudentProfile.java

--- src/main/java/com/example/studentservice/model/StudentProfile.java
+++ src/main/java/com/example/studentservice/model/StudentProfile.java
@@ -1,5 +1,7 @@
 package com.example.studentservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,20 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StudentProfile {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
+
+	@JsonProperty("first_name")
 	private String firstName;
+
+	@JsonProperty("last_name")
 	private String lastName;
+
+	@JsonProperty("phone_number")
 	private String phoneNumber;
 
 	@Column(unique=true)
