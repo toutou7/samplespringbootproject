9f77b0264d04c0bfacc4b4ac5b0851856f90b02f
toutou7 <yangvitou999@gmail.com>
2022-02-21 14:30:40 +0700

    I wanna make some change.

---
+++ src/main/java/com/example/studentservice/model/StudentProfile.java
@@ -0,0 +1,27 @@
+package com.example.studentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class StudentProfile {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+
+	@Column(unique=true)
+	private String email;
+
+	//updated
+}
